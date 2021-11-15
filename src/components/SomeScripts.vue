f070010df0b43510c7a78176815361f68bd7eea5
Datuluna Ali Dilangalen <datuluna.dilangalen@paymaya.com>
2021-11-15 12:30:09 +0800

    bunch of changes, see code

---
+++ src/components/SomeScripts.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="some-scripts">
+    <button @click="sayToAndroid('Hello From Mini App')">Say Hello!</button>
+    <button @click="someEventFunc()">Some Event Button</button>
+    <button @click="sendData()">Send Some Data</button>
+    <button @click="getGeolcation()">Get Geolocation</button>
+    <button @click="getLatestLocation()">Get Geolocation V2</button>
+  </div>
+</template>
+
+<script>
+window.androidObj = function AndroidClass () {}
+
+export default {
+  methods: {
+    sayToAndroid (message) {
+      console.log('Hello World!')
+      window.JSBridge.showMessageInNative(message)
+    },
+    someEventFunc () {
+      console.log('The End is Worlding')
+      window.JSBridge.call('someEventInMiniApp#12321')
+    },
+    sendData () {
+      var response
+      const payload = {
+        notificationID: 'someUUID',
+        message: 'Transaction Successful'
+      }
+      response = window.JSBridge.receiveData(JSON.stringify(payload))
+      console.log(response.concat(' Concat-ed this string in mini app client'))
+    },
+    getGeolcation () {
+      var response
+      response = window.JSBridge.displayGeolocation('send me geolocation')
+      console.log(response)
+    },
+    getLatestLocation () {
+      const request = {
+        miniAppID: '0bf409ca-c677-4bb5-821e-d1a49745636d',
+        message: 'Request for latest location'
+      }
+      var response = window.JSBridge.fetchLatestLocation(JSON.stringify(request))
+      console.log('Latest Location: '.concat(response))
+      alert(response)
+    }
+
+  }
+}
+</script>
+
+<!-- <style>
+body {
+  background-color: #93b874;
+}
+</style> -->
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <SomeScripts />
   </div>
 </template>
+
+<script>
+import SomeScripts from '@/components/SomeScripts.vue'
+
+export default {
+  name: 'About',
+  components: {
+    SomeScripts
+  }
+}
+</script>
